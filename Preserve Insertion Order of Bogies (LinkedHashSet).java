f94255938aa7082f32eaa5646d06938ce5a33554
Praveen <praveensuresh25082006@gmail.com>
2026-04-17 16:22:21 +0530

    Create Preserve Insertion Order of Bogies (LinkedHashSet).java

---
+++ Preserve Insertion Order of Bogies (LinkedHashSet).java
@@ -0,0 +1,19 @@
+import java.util.LinkedHashSet;
+
+public class TrainConsistUC5 {
+
+    public static void main(String[] args) {
+        LinkedHashSet<String> train = new LinkedHashSet<>();
+
+        train.add("Engine");
+        train.add("Sleeper");
+        train.add("Cargo");
+        train.add("Guard");
+
+        train.add("Sleeper"); 
+        System.out.println("Final Train Formation:");
+        for (String bogie : train) {
+            System.out.println(bogie);
+        }
+    }
+}
