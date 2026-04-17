01a5d1c9b8068c74baf8cfbd527d2acd8c192723
Praveen <praveensuresh25082006@gmail.com>
2026-04-18 02:04:09 +0530

    Create Linear Search for Bogie ID (Array-Based Searching).java

---
+++ Linear Search for Bogie ID (Array-Based Searching).java
@@ -0,0 +1,29 @@
+import java.util.*;
+public class TrainConsistApp {
+    public static boolean linearSearch(String[] bogieIds, String key) {
+        for (String id : bogieIds) {
+            if (id.equals(key)) {
+                return true; 
+            }
+        }
+
+        return false; 
+    }
+
+    public static void main(String[] args) {
+        String[] bogieIds = {
+            "BG101", "BG205", "BG309", "BG412", "BG550"
+        };
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter Bogie ID to search: ");
+        String searchKey = sc.nextLine();
+        boolean found = linearSearch(bogieIds, searchKey);
+        if (found) {
+            System.out.println("Bogie ID FOUND ✅");
+        } else {
+            System.out.println("Bogie ID NOT FOUND ❌");
+        }
+
+        sc.close();
+    }
+}
