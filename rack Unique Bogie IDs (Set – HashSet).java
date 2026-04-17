e8a3c1441fdb211acf0634fedb0836151e4e6fd6
Praveen <praveensuresh25082006@gmail.com>
2026-04-17 16:14:40 +0530

    Add TrainConsistUC3 to manage unique bogie IDs

---
+++ rack Unique Bogie IDs (Set – HashSet).java
@@ -0,0 +1,19 @@
+import java.util.HashSet;
+import java.util.Set;
+
+public class TrainConsistUC3 {
+
+    public static void main(String[] args) {
+        Set<String> bogieIds = new HashSet<>();
+        bogieIds.add("BG101");
+        bogieIds.add("BG102");
+        bogieIds.add("BG103");
+        bogieIds.add("BG101"); 
+        bogieIds.add("BG104");
+        bogieIds.add("BG102"); 
+        System.out.println("Unique Bogie IDs in Train:");
+        for (String id : bogieIds) {
+            System.out.println(id);
+        }
+    }
+}
